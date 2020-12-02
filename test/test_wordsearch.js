5c78af48c5fabf5c96e6c9b47847c7f72578d3db
zahra-hm <62305173+zahra-hm@users.noreply.github.com>
2020-12-02 17:34:43 -0500

    added test cases for backwards checks

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
-
 const wordSearch = require('../wordsearch.js')
-
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
@@ -16,10 +14,8 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
-
     assert.isFalse(result);
   });
-
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -32,7 +28,48 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'C', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'Y', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'NANCY')
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'B', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'A', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'C', 'A', 'P', 'A', 'I'],
+      ['E', 'V', 'A', 'C', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CAVE')
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'B', 'Y', 'L', 'R', 'L'],
+      ['B', 'F', 'R', 'A', 'N', 'P', 'Y', 'B'],
+      ['U', 'B', 'T', 'C', 'A', 'P', 'A', 'I'],
+      ['E', 'V', 'A', 'C', 'K', 'A', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
     assert.isTrue(result);
   });
 });
